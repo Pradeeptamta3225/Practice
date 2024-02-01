56547b281bceb138f82ed9ad0432347547a5281b
Pradeep Tamta <118155647+Pradeeptamta3225@users.noreply.github.com>
2024-02-01 22:20:58 +0530

    practice string topic

--- Basic/script.js
+++ Basic/script.js
@@ -32,22 +32,35 @@
 
 //stack(primitive) and heap(Non-primitive) 
 
-let myName = "Pradeep Tamta"
+// let myName = "Pradeep Tamta"
 
-let alternateName = myName
-alternateName = "Tamta"
+// let alternateName = myName
+// alternateName = "Tamta"
 
-console.log(myName);
-console.log(alternateName);
+// console.log(myName);
+// console.log(alternateName);
 
 
-let userOne = {
-  email: "pradeep@gmail.com",
-  age: 24
-}
+// let userOne = {
+//   email: "pradeep@gmail.com",
+//   age: 24
+// }
 
-let userTwo = userOne;
+// let userTwo = userOne;
 
-userTwo.email = "tamta@gmail.com";
-console.log(userOne);
-console.log(userTwo);
+// userTwo.email = "tamta@gmail.com";
+// console.log(userOne);
+// console.log(userTwo);
+
+
+let name = "Pradeep";
+let repoCount = 50
+
+
+console.log(`Hello My Name is ${name} and my repocount is ${repoCount}`);
+
+let gameName = new String("Pradeep");
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.charAt(3));
+console.log(gameName.toUpperCase())
